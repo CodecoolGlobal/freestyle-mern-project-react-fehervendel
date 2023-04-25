03ef65654911ba21121a5cc22a78d1c913254289
stszandra <snszandra@gmail.com>
2023-04-25 16:14:07 +0200

    home page design early phase

--- client/src/components/games/Game.js
+++ client/src/components/games/Game.js
@@ -2,28 +2,28 @@ import "./css/Game.css";
 
 function Game(props) {
   return (
-    <div class="game">
-      <div id="gameContainer">
-        <img src={props.game.background_image} id="backGroundImage"></img>
-        <div id="gameContainerSecondColumn">
-          <div id="mainPageGameName">{props.game.name}</div>
-        </div>
-      </div>
-    </div>
-
-    // <div id="gameContainer">
-    //   <img src={props.game.background_image} id="backGroundImage"></img>
-    //   <div id="gameContainerSecondColumn">
-    //     <div id="mainPageGameName">{props.game.name}</div>
-    //     <div id="screenshotsContainer">
-    //       <img src={props.game.short_screenshots[1].image} className="screenshot"></img>
-    //       <img src={props.game.short_screenshots[2].image} className="screenshot"></img>
-    //       <img src={props.game.short_screenshots[3].image} className="screenshot"></img>
-    //       <img src={props.game.short_screenshots[4].image} className="screenshot"></img>
+    // <div class="game">
+    //   <div id="gameContainer">
+    //     <img src={props.game.background_image} id="backGroundImage"></img>
+    //     <div id="gameContainerSecondColumn">
+    //       <div id="mainPageGameName">{props.game.name}</div>
     //     </div>
-    //     <div>Price</div>
     //   </div>
     // </div>
+
+    <div id="gameContainer">
+      <img src={props.game.background_image} id="backGroundImage"></img>
+      <div id="gameContainerSecondColumn">
+        <div id="mainPageGameName">{props.game.name}</div>
+        <div id="screenshotsContainer">
+          <img src={props.game.short_screenshots[1].image} className="screenshot"></img>
+          <img src={props.game.short_screenshots[2].image} className="screenshot"></img>
+          <img src={props.game.short_screenshots[3].image} className="screenshot"></img>
+          <img src={props.game.short_screenshots[4].image} className="screenshot"></img>
+        </div>
+        <div>Price</div>
+      </div>
+    </div>
   );
 }
 
