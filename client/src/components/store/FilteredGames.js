ca2a7537b12889ddd5a9936f66e82c812cfaa5a0
fehervendel <fehervendel@gmail.com>
2023-04-26 10:26:10 +0200

    Fetching all games

--- client/src/components/store/FilteredGames.js
+++ client/src/components/store/FilteredGames.js
@@ -1,8 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import FilteredGame from "./AllGamesPage";
 
-function FilteredGames() {
-  return null;
+function FilteredGames({searchFor}) {
+  const [allGames, setAllGames] = useState([]);
+
+  useEffect(() => {  
+    async function getGames() {
+      try {
+        const res = await fetch("http://localhost:3001/api/allgames");
+        const response = await res.json();
+        setAllGames(response);
+      } catch (error) {
+        console.log(`Fetching games failed: ${error}`);
+      }
+    }
+    getGames();
+  },[])
+
+  async function getGames() {
+    try {
+      const res = await fetch("http://localhost:3001/api/allgames");
+      const response = await res.json();
+      setAllGames(response);
+    } catch (error) {
+      console.log(`Fetching games failed: ${error}`);
+    }
+  }
+  
+  return (null)
 }
 
 export default FilteredGames;
--- client/src/components/store/SearchBar.js
+++ client/src/components/store/SearchBar.js
@@ -1,10 +1,22 @@
 import React, { useState } from "react";
 
-function SearchBar() {
+function SearchBar({setFilteredGames, setSearchFor}) {
+  const [searchText, setSearchText] = useState("");
+
+  function switchToFilteredGames() {
+    setFilteredGames(true);
+    setSearchFor(searchText);
+  }
+
+  function search(e){
+    e.preventDefault();
+    setSearchText(e.target.value);
+  }
+
   return (
     <div>
-      <input id="searchBar" type="text" placeholder="Search"></input>
-      <button>Search</button>
+      <input id="searchBar" type="text" placeholder="Search" onChange={(e) => search(e)}></input>
+      <button onClick={() => switchToFilteredGames()}>Search</button>
     </div>
   );
 }
