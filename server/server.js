6fd7912b3165f987dce053667e105e1d9bde4cbd
stszandra <snszandra@gmail.com>
2023-04-26 21:05:18 +0200

    price calculated for each game and updated mongodb

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import User from "./model/user.js";
 import Game from "./model/game.js";
+import gameSchema from "./model/game.js";
 import crypto from "crypto";
 
 const app = express();
@@ -41,17 +42,14 @@ app.get("/api/allgames", async (req, res) => {
 app.post("/api/userlogin", async (req, res) => {
   try {
     const userName = req.body.userName;
-    const userPassword = crypto.createHash('sha256').update(req.body.userPassword).digest('hex');
+    const userPassword = crypto.createHash("sha256").update(req.body.userPassword).digest("hex");
     const user = await User.find({ userName: userName });
-    
-    
-      if(user.length > 0 && user[0].userPassword === userPassword){
-        res.status(200).json({ access: 1, email: user[0].userEmail});
-      } else {
-        res.status(401).json(0);
-      }
 
-    
+    if (user.length > 0 && user[0].userPassword === userPassword) {
+      res.status(200).json({ access: 1, email: user[0].userEmail });
+    } else {
+      res.status(401).json(0);
+    }
   } catch (error) {
     console.log(error);
     res.status(500).send("Something went wrong");
@@ -61,55 +59,91 @@ app.post("/api/userlogin", async (req, res) => {
 app.post("/api/userregistration", async (req, res) => {
   //+feature pattern check for inputfields email->@
   const userName = req.body.userName;
-  const userPassword = crypto.createHash('sha256').update(req.body.userPassword).digest('hex');
+  const userPassword = crypto.createHash("sha256").update(req.body.userPassword).digest("hex");
   const userEmail = req.body.userEmail;
-  const userPasswordAgain = crypto.createHash('sha256').update(req.body.userPasswordAgain).digest('hex');
+  const userPasswordAgain = crypto.createHash("sha256").update(req.body.userPasswordAgain).digest("hex");
   const date = Date.now();
-  const userNameCheck = await User.find({userName: userName});
-  const userEmailCheck = await User.find({userEmail: userEmail});
+  const userNameCheck = await User.find({ userName: userName });
+  const userEmailCheck = await User.find({ userEmail: userEmail });
 
-  
-    if(userName.length > 0 && userPassword.length > 0 && userEmail.length > 0 && userPasswordAgain.length > 0){
-
-      if(userPassword === userPasswordAgain){
-
-        if(userNameCheck.length === 0 && userEmailCheck.length === 0){
-        
-          const user = new User({
-            userName,
-            userPassword,
-            userPasswordAgain,
-            userEmail,
-            date,
-          });
-          user
-            .save()
-            .then(() => res.status(200).send("1"))
-            .catch((err) => res.status(400).json({ success: false }));
-        }else{
-          res.status(401).send("0"); 
-          //console.log("Username or Email already exists!");
-        }
-      }else{
-          res.status(400).send("2");
-         // console.log("Given passwords are not the same!");
-      }    
-        
+  if (userName.length > 0 && userPassword.length > 0 && userEmail.length > 0 && userPasswordAgain.length > 0) {
+    if (userPassword === userPasswordAgain) {
+      if (userNameCheck.length === 0 && userEmailCheck.length === 0) {
+        const user = new User({
+          userName,
+          userPassword,
+          userPasswordAgain,
+          userEmail,
+          date,
+        });
+        user
+          .save()
+          .then(() => res.status(200).send("1"))
+          .catch((err) => res.status(400).json({ success: false }));
+      } else {
+        res.status(401).send("0");
+        //console.log("Username or Email already exists!");
+      }
+    } else {
+      res.status(400).send("2");
+      // console.log("Given passwords are not the same!");
+    }
   } else {
     res.status(400).send("3");
-   // console.log("Fill the fields!");
+    // console.log("Fill the fields!");
   }
+});
+
+const fetchData = async (pageNum) => {
+  const response = await (
+    await fetch(`https://api.rawg.io/api/games?key=23dd7ede08ea4476afb8fb8286843785&page=${pageNum}&page_size=40`)
+  ).json();
+  Game.create(response.results);
+};
+
+for (let i = 1; i < 31; i++) {
+  //fetchData(i);
+}
+for (let i = 31; i < 61; i++) {
+  //fetchData(i);
+}
+
+const updateGamesWithPrice = async () => {
+  let games = await gameSchema.find();
+  games = games.map(async (game) => {
+    const price = generatePriceBasedOnRatingAndReleaseDate(game.released, game.rating, game.tags);
+    game.price = price;
+    await game.save();
+  });
+};
+//updateGamesWithPrice();
+
+function generatePriceBasedOnRatingAndReleaseDate(releaseDate, rating, tags) {
+  const yearsUntilToday = Math.round(new Date("2023-04-28") / 31536000000);
+  const yearsUntilReleaseDate = Math.round(new Date(releaseDate) / 31536000000);
+  const age = yearsUntilToday - yearsUntilReleaseDate;
+
+  let price = 0;
+  if (rating < 3) {
+    price = 5.99;
+  } else if (rating >= 3 && rating < 3.6) {
+    price = 9.99;
+  } else if (rating >= 3.6 && age > 8) {
+    price = 19.99;
+  } else if (rating >= 3.6 && age > 4) {
+    price = 29.99;
+  } else if (rating >= 3.6) {
+    price = 59.99;
+  }
+  tags.forEach((tag) => {
+    if (tag.name === "Free to Play") {
+      price = 0;
+    }
   });
 
-// const fetchData = async (pageNum) => {
-//   const response = await (
-//     await fetch(`https://api.rawg.io/api/games?key=23dd7ede08ea4476afb8fb8286843785&page=${pageNum}&page_size=40`)
-//   ).json();
-//   Game.create(response.results);
-// };
-// for (let i = 1; i < 31; i++) {
-//   fetchData(i);
-// }
+  return price;
+}
+
 //(userName !== userNameCheck[0].userName && userEmail !== userEmailCheck[0].userEmail)
 mongoose
   .connect("mongodb+srv://lorikpatrik:7a8r4K01@cluster0.bu8nsrn.mongodb.net/project")
